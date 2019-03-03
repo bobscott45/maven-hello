9f594f3114c80df1506e456b9e55218628a34af7
Bob <bob@rwscott.co.uk>
2019-03-03 12:35:54 +0000

    Remove WordUtils

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.text.WordUtils;
 
-public class HelloWorld {
+
+public class commit -aHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(StringUtils.capitalize("hello world"));
-        System.out.println(WordUtils.capitalize("from word utils"));
     }
 }
